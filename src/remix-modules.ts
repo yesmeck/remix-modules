9f453392a5c2cfb9c27ff429b98e699d3601f50b
Wei Zhu <yesmeck@gmail.com>
2024-02-04 21:16:35 +1030

    wip

--- src/remix-modules.ts
+++ src/remix-modules.ts
@@ -1,77 +1,17 @@
-import { RouteManifest } from '@remix-run/dev/dist/config/routes';
-import { existsSync } from 'fs';
-import { resolve, relative, join } from 'path';
-
-
-let flatRoutes: typeof import('@remix-run/dev/dist/config/flat-routes').flatRoutes;
-
-try {
-  flatRoutes = require('@remix-run/dev/dist/config/flat-routes').flatRoutes;
-} catch (e) {
-  flatRoutes = require('@vercel/remix-run-dev/dist/config/flat-routes').flatRoutes;
-}
-
-const entryExts = [".js", ".jsx", ".ts", ".tsx"];
-
-function findEntry(dir: string, basename: string): string | undefined {
-  for (let ext of entryExts) {
-    let file = resolve(dir, basename + ext);
-    if (existsSync(file)) return relative(dir, file);
-  }
-
-  return undefined;
-}
-
+import { Options, RemixApp } from './remix-app';
 
 export class RemixModules {
-  #routes: RouteManifest = {};
+  #apps: RemixApp[] = [];
 
-  async mount(module: string, path: string) {
-    const toPath = path.replace(/^\//, "");
-    const moduleDir = resolve(process.cwd(), 'app', module);
-    const rootRouteFile = findEntry(moduleDir, 'root');
-
-    if (!existsSync(resolve(moduleDir, 'routes'))) {
-      return;
-    }
-
-    const routes: RouteManifest = {};
-
-    const rootRouteId = `${module}/root`;
-
-    if (rootRouteFile) {
-      routes[rootRouteId] = {
-        path: toPath,
-        id: rootRouteId,
-        file: join(module, rootRouteFile),
-        parentId: 'root'
-      };
-    }
-
-    const fileRoutes = await flatRoutes(moduleDir);
-
-    for (let route of Object.values(fileRoutes)) {
-      console.log(route);
-      const id = `${module}/${route.id}`;
-
-      route.id = id;
-      route.file = join(module, route.file);
-      route.parentId =
-        route.parentId === 'root' ?
-          rootRouteFile ?
-            rootRouteId : 'root'
-          : `${module}/${route.parentId}`;
-
-      routes[route.id] = { ...route, parentId: route.parentId };
-    }
-
-    this.#routes = {
-      ...this.#routes,
-      ...routes
-    };
+  mount(path: string, option: Options) {
+    const app = new RemixApp(path, option);
+    this.#apps.push(app);
   }
 
-  routes() {
-    return this.#routes;
+  async routes() {
+    return this.#apps.reduce(async (routes, app) => {
+      const appRoutes = await app.routes();
+      return { ...routes, ...appRoutes };
+    }, {});
   }
 }
